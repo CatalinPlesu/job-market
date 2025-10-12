91b0206a489bdb9f414edf70e79df626f9f90382
Catalin Plesu <catalinhimself@gmail.com>
2025-10-12 21:08:45 +0300

    basic config

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ class Config:
         "https://www.rabota.md/ro/jobs-moldova",
         "https://www.delucru.md/jobs"
     ]
+    env_api_key = "LLM_API_KEY"
+    llm_api = "https://openrouter.ai/api/v1"
+    llm_model = "deepseek/deepseek-chat-v3.1:free"
